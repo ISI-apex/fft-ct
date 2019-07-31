3605af09511f2fcfc074da9d9a380c7d9c44d8b2
Kaushik Datta <kdatta@isi.edu>
2019-07-31 09:24:58 -0400

    Add transpose_flt_blocked()

--- transpose.h
+++ transpose.h
@@ -16,6 +16,10 @@
 void transpose_flt_naive(const float* restrict A, float* restrict B,
 			 size_t A_rows, size_t A_cols);
 
+void transpose_flt_blocked(const float* restrict A, float* restrict B,
+			   size_t A_rows, size_t A_cols,
+			   size_t blk_rows, size_t blk_cols);
+
 void transpose_dbl_naive(const double* restrict A, double* restrict B,
 			 size_t A_rows, size_t A_cols);
 
