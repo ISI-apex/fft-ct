57508db0c54efed600400d64a6d3fde0634c104e
Connor Imes <cimes@isi.edu>
2019-08-02 08:46:39 -0400

    transpose-mkl: add float and MKL_Complex8 functions

--- transpose-mkl.c
+++ transpose-mkl.c
@@ -10,10 +10,27 @@
 
 #include "transpose-mkl.h"
 
+void transpose_flt_mkl(const float* restrict A, float* restrict B,
+                       size_t A_rows, size_t A_cols)
+{
+    mkl_somatcopy('r', 't', A_rows, A_cols, 1, A, A_cols, B, A_rows);
+}
+
 void transpose_dbl_mkl(const double* restrict A, double* restrict B,
                        size_t A_rows, size_t A_cols)
 {
     mkl_domatcopy('r', 't', A_rows, A_cols, 1, A, A_cols, B, A_rows);
+}
+
+void transpose_cmplx8_mkl(const MKL_Complex8* restrict A,
+                          MKL_Complex8* restrict B,
+                          size_t A_rows, size_t A_cols)
+{
+    static const MKL_Complex8 alpha = {
+        .real = 1,
+        .imag = 0,
+    };
+    mkl_comatcopy('r', 't', A_rows, A_cols, alpha, A, A_cols, B, A_rows);
 }
 
 void transpose_cmplx16_mkl(const MKL_Complex16* restrict A,
--- transpose-mkl.h
+++ transpose-mkl.h
@@ -11,8 +11,15 @@
 
 #include <mkl.h>
 
+void transpose_flt_mkl(const float* restrict A, float* restrict B,
+                       size_t A_rows, size_t A_cols);
+
 void transpose_dbl_mkl(const double* restrict A, double* restrict B,
                        size_t A_rows, size_t A_cols);
+
+void transpose_cmplx8_mkl(const MKL_Complex8* restrict A,
+                          MKL_Complex8* restrict B,
+                          size_t A_rows, size_t A_cols);
 
 void transpose_cmplx16_mkl(const MKL_Complex16* restrict A,
                            MKL_Complex16* restrict B,
