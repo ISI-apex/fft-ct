4694e4c51dfd7ff894c2a43dff1986f165f310a3
Connor Imes <cimes@isi.edu>
2019-08-02 08:45:59 -0400

    util-mkl: add MKL_Complex8 type functions

--- util-mkl.c
+++ util-mkl.c
@@ -12,12 +12,33 @@
 #include "util.h"
 #include "util-mkl.h"
 
+void fill_rand_cmplx8(MKL_Complex8 *a, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++) {
+        a[i].real = rand_flt();
+        a[i].imag = rand_flt();
+    }
+}
+
 void fill_rand_cmplx16(MKL_Complex16 *a, size_t len)
 {
     size_t i;
     for (i = 0; i < len; i++) {
         a[i].real = rand_dbl();
         a[i].imag = rand_dbl();
+    }
+}
+
+void matrix_print_cmplx8(const MKL_Complex8 *A, size_t nrows, size_t ncols)
+{
+    size_t r, c, i;
+    for (r = 0; r < nrows; r++) {
+        for (c = 0; c < ncols; c++) {
+            i = r * ncols + c;
+            printf("%s(%f, %f)", (c > 0 ? ", " : ""), A[i].real, A[i].imag);
+        }
+        printf("\n");
     }
 }
 
@@ -31,6 +52,11 @@ void matrix_print_cmplx16(const MKL_Complex16 *A, size_t nrows, size_t ncols)
         }
         printf("\n");
     }
+}
+
+int is_eq_cmplx8(const MKL_Complex8 a, const MKL_Complex8 b)
+{
+    return is_eq_flt(a.real, b.real) && is_eq_flt(a.imag, b.imag);
 }
 
 int is_eq_cmplx16(const MKL_Complex16 a, const MKL_Complex16 b)
--- util-mkl.h
+++ util-mkl.h
@@ -11,10 +11,13 @@
 
 #include <mkl.h>
 
+void fill_rand_cmplx8(MKL_Complex8 *a, size_t len);
 void fill_rand_cmplx16(MKL_Complex16 *a, size_t len);
 
+void matrix_print_cmplx8(const MKL_Complex8 *A, size_t nrows, size_t ncols);
 void matrix_print_cmplx16(const MKL_Complex16 *A, size_t nrows, size_t ncols);
 
+int is_eq_cmplx8(const MKL_Complex8 a, const MKL_Complex8 b);
 int is_eq_cmplx16(const MKL_Complex16 a, const MKL_Complex16 b);
 
 #endif /* UTIL_MKL_H */
