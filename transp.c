cd71ba809c1a0e3c895dd0cbf813b3414d3d2844
Connor Imes <cimes@isi.edu>
2019-08-05 11:53:15 -0400

    transp: wrap functionality in macro

--- transp.c
+++ transp.c
@@ -16,14 +16,19 @@
 #include "util.h"
 #include "util-fftw.h"
 
-static void transp_fftw_complex_naive(size_t nrows, size_t ncols)
-{
-    fftw_complex *A = assert_fftw_malloc(nrows * ncols * sizeof(fftw_complex));
-    fftw_complex *B = assert_fftw_malloc(nrows * ncols * sizeof(fftw_complex));
-    fill_rand_fftw_complex(A, nrows * ncols);
-    transpose_fftw_complex_naive(A, B, nrows, ncols);
-    fftw_free(B);
-    fftw_free(A);
+#define TRANSP_SETUP(datatype, fn_malloc, fn_fill, nrows, ncols) \
+    datatype *A = fn_malloc(nrows * ncols * sizeof(datatype)); \
+    datatype *B = fn_malloc(nrows * ncols * sizeof(datatype)); \
+    fn_fill(A, nrows * ncols);
+
+#define TRANSP_TEARDOWN(A, B, fn_free) \
+    fn_free(B); \
+    fn_free(A);
+
+#define TRANSP(datatype, fn_malloc, fn_free, fn_fill, fn_transp, nrows, ncols) { \
+    TRANSP_SETUP(datatype, fn_malloc, fn_fill, nrows, ncols); \
+    fn_transp(A, B, nrows, ncols); \
+    TRANSP_TEARDOWN(A, B, fn_free); \
 }
 
 static void usage(void)
@@ -43,6 +48,10 @@ int main(int argc, char **argv)
         fprintf(stderr, "Parameters nrows and ncols must be > 0\n");
         return EINVAL;
     }
-    transp_fftw_complex_naive(nrows, ncols);
+#if defined(USE_FFTW_NAIVE)
+    TRANSP(fftw_complex, assert_fftw_malloc, fftw_free,
+           fill_rand_fftw_complex, transpose_fftw_complex_naive,
+           nrows, ncols);
+#endif
     return 0;
 }
