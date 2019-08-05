24fe922abc2a16a8f8a9fa08d76e8c60e4e90f63
Connor Imes <cimes@isi.edu>
2019-08-05 15:09:01 -0400

    transp: add MKL float and double transposes

--- transp.c
+++ transp.c
@@ -19,6 +19,12 @@
 #include "util-fftw.h"
 #endif
 
+#if defined(USE_MKL_FLOAT) || defined(USE_MKL_DOUBLE)
+#include <mkl.h>
+#include "transpose-mkl.h"
+#include "util-mkl.h"
+#endif
+
 #define TRANSP_SETUP(datatype, fn_malloc, fn_fill, nrows, ncols) \
     datatype *A = fn_malloc(nrows * ncols * sizeof(datatype)); \
     datatype *B = fn_malloc(nrows * ncols * sizeof(datatype)); \
@@ -51,6 +57,7 @@ int main(int argc, char **argv)
         fprintf(stderr, "Parameters nrows and ncols must be > 0\n");
         return EINVAL;
     }
+// TODO: Add float and double transposes (naive and blocked), MKL transposes
 #if defined(USE_FLOAT_NAIVE)
     TRANSP(float, assert_malloc, free,
            fill_rand_flt, transpose_flt_naive,
@@ -62,6 +69,14 @@ int main(int argc, char **argv)
 #elif defined(USE_FFTW_NAIVE)
     TRANSP(fftw_complex, assert_fftw_malloc, fftw_free,
            fill_rand_fftw_complex, transpose_fftw_complex_naive,
+           nrows, ncols);
+#elif defined(USE_MKL_FLOAT)
+    TRANSP(float, assert_malloc, free,
+           fill_rand_flt, transpose_flt_mkl,
+           nrows, ncols);
+#elif defined(USE_MKL_DOUBLE)
+    TRANSP(double, assert_malloc, free,
+           fill_rand_dbl, transpose_dbl_mkl,
            nrows, ncols);
 #else
     #error "No matching transpose implementation found!"
